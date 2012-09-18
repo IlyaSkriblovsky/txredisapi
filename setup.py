0e72db5096db9c9cbceaec39f40541413d1b2e75
fiorix <fiorix@gmail.com>
2012-09-18 13:30:20 -0400

    v=0.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 
 setuptools.setup(
     name="txredisapi",
-    version="0.8",
+    version="0.9",
     py_modules=["txredisapi"],
     install_requires=["twisted"],
     author="Alexandre Fiori",
